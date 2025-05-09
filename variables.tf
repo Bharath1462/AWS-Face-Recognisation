5691c40617342581567a9573bbd614b7d0c2adb3
Bharath1462 <124774580+Bharath1462@users.noreply.github.com>
2025-05-09 21:39:30 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,6 +6,6 @@ variable "s3_bucket_name" {
   default = "aws-face-recognisation" # use a globally unique name
 }
 
-variable "rekognition_collection_id" {
+variable "recognition_collection_id" {
   default = "face-collection-demo"
 }
