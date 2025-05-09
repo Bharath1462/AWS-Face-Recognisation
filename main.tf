5e9b02d44219ba4e2dcb8b5020747e265bd82d9e
Bharath1462 <124774580+Bharath1462@users.noreply.github.com>
2025-05-09 21:38:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,18 +3,18 @@ provider "aws" {
 }
 
 # Create S3 Bucket
-resource "aws_s3_bucket" "rekognition_bucket" {
+resource "aws_s3_bucket" "recognition_bucket" {
   bucket = var.s3_bucket_name
 
   tags = {
-    Name        = "Rekognition Image Bucket"
+    Name        = "Recognition Image Bucket"
     Environment = "Dev"
   }
 }
 
-# Rekognition IAM Role
-resource "aws_iam_role" "rekognition_role" {
-  name = "rekognition_s3_access_role"
+# Recognition IAM Role
+resource "aws_iam_role" "recognition_role" {
+  name = "recognition_s3_access_role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -22,16 +22,16 @@ resource "aws_iam_role" "rekognition_role" {
       Action    = "sts:AssumeRole",
       Effect    = "Allow",
       Principal = {
-        Service = "rekognition.amazonaws.com"
+        Service = "recognition.amazonaws.com"
       }
     }]
   })
 }
 
-# Attach Policy to allow Rekognition to read from S3
-resource "aws_iam_policy" "rekognition_policy" {
-  name        = "RekognitionS3AccessPolicy"
-  description = "Allows Rekognition to access S3 bucket"
+# Attach Policy to allow Recognition to read from S3
+resource "aws_iam_policy" "recognition_policy" {
+  name        = "RecognitionS3AccessPolicy"
+  description = "Allows Recognition to access S3 bucket"
   policy      = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -42,20 +42,20 @@ resource "aws_iam_policy" "rekognition_policy" {
           "s3:ListBucket"
         ],
         Resource = [
-          "${aws_s3_bucket.rekognition_bucket.arn}",
-          "${aws_s3_bucket.rekognition_bucket.arn}/*"
+          "${aws_s3_bucket.recognition_bucket.arn}",
+          "${aws_s3_bucket.recognition_bucket.arn}/*"
         ]
       }
     ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "rekognition_policy_attach" {
-  role       = aws_iam_role.rekognition_role.name
-  policy_arn = aws_iam_policy.rekognition_policy.arn
+resource "aws_iam_role_policy_attachment" "recognition_policy_attach" {
+  role       = aws_iam_role.recognition_role.name
+  policy_arn = aws_iam_policy.recognition_policy.arn
 }
 
-# Rekognition Collection
-resource "aws_rekognition_collection" "face_collection" {
-  collection_id = var.rekognition_collection_id
+# Recognition Collection
+resource "aws_recognition_collection" "face_collection" {
+  collection_id = var.recognition_collection_id
 }
