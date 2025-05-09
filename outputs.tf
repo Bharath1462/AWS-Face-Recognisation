4befb41f97b1a428056be5a91fa12ca9be5c04fa
Bharath1462 <124774580+Bharath1462@users.noreply.github.com>
2025-05-09 21:39:02 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "s3_bucket_name" {
-  value = aws_s3_bucket.rekognition_bucket.bucket
+  value = aws_s3_bucket.recognition_bucket.bucket
 }
 
-output "rekognition_collection_id" {
-  value = aws_rekognition_collection.face_collection.collection_id
+output "recognition_collection_id" {
+  value = aws_recognition_collection.face_collection.collection_id
 }
